46555384f6c5220a5b2d2e223c87c23bc34db3f1
abram-oconnor <93237500+abram-oconnor@users.noreply.github.com>
2023-02-28 21:16:24 -0800

    added allow all egress TCP

--- security-groups/create-webserver-security-group.tf
+++ security-groups/create-webserver-security-group.tf
@@ -15,6 +15,14 @@ resource "aws_security_group" "WebServerTest" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  // Allow all outgoing TCP traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "WebServerTest"
   }
